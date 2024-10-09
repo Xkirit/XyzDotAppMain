682f71f5e89bc81e9fbe2d8108ea7b238fb796b3
Kirit <kiritrajut@gmail.com>
2024-10-09 09:35:41 +0530

    temp land

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page(){
     <div className="justify center  items-center text-center h-screen w-full flex bg-gray-950">
       <div className="text-white font-mono text-center justify-center items-center flex-col gap-10 w-screen z-50">
         {/* <h2 className="text-center items-center justify-center flex" > Under Construction...</h2> */}
-        <div className="text-center items-center justify-center flex mt-5 z-50">
+        <div className="text-center items-center justify-center flex z-50">
         <Link href="/App" > <Button className="bg-purple-700 items-center justify-center flex z-50">EnterApp</Button> </Link>
         </div>
         
