fdb9573e4f1318b363cf790eaf9717b4b9123492
Kirit <kiritrajut@gmail.com>
2024-10-09 14:37:26 +0530

    ignor

--- src/components/ui/posts/Post.tsx
+++ src/components/ui/posts/Post.tsx
@@ -30,21 +30,21 @@ export default function Post({ post }: PostProps) {
       <div className="flex justify-between gap-3">
         <div className="flex flex-wrap gap-3">
           <UserTooltip user={post.user}>
-            <Link href={`/users/${post.user.username}`}>
+            <Link href={`/App/users/${post.user.username}`}>
               <UserAvatar avatarUrl={post.user.avatarUrl} />
             </Link>
           </UserTooltip>
           <div>
             <UserTooltip user={post.user}>
               <Link
-                href={`/users/${post.user.username}`}
+                href={`/App/users/${post.user.username}`}
                 className="block font-medium hover:underline"
               >
                 {post.user.displayName}
               </Link>
             </UserTooltip>
             <Link
-              href={`/posts/${post.id}`}
+              href={`/App/posts/${post.id}`}
               className="block text-sm text-muted-foreground hover:underline"
               suppressHydrationWarning
             >
